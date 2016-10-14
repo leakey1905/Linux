2c74c2db632682f307239dd62315fc948756216c
leakey1905 <leakey1905@users.noreply.github.com>
2016-10-15 01:06:40 +0800

    basic commands to change the
    change the resolution of the screen 1
    ref: http://blog.csdn.net/zhxp_870516/article/details/8663366
    change to 2560*1080 to fit the 21:9 screen
    But it cannot be used automatically when system starts

---
+++ ChangeScreenSolution.sh
@@ -0,0 +1,3 @@
+sudo xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
+sudo xrandr --addmode HDMI1 2560x1080_60.00
+sudo xrandr --output HDMI1 --mode 2560x1080_60.00
