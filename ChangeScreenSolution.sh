ff1bcc2b942d52cf73446f69e0d5ebafc4be5a45
leakey1905 <leakey1905@users.noreply.github.com>
2016-10-19 21:37:43 +0800

    a successful try
    Use 70hz then set the screen as second screen, then it works!

--- ChangeScreenSolution.sh
+++ ChangeScreenSolution.sh
@@ -1,3 +1,4 @@
+cvt 2560 1080 60
 sudo xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
 sudo xrandr --addmode HDMI1 2560x1080_60.00
 sudo xrandr --output HDMI1 --mode 2560x1080_60.00
