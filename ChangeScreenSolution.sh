ccbd821da70fdefbe4a5ebe54e371913788aca22
leakey1905 <leakey1905@users.noreply.github.com>
2016-11-22 22:36:03 +0800

    Update ChangeScreenSolution.sh

--- ChangeScreenSolution.sh
+++ ChangeScreenSolution.sh
@@ -2,3 +2,5 @@ cvt 2560 1080 60
 sudo xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
 sudo xrandr --addmode HDMI1 2560x1080_60.00
 sudo xrandr --output HDMI1 --mode 2560x1080_60.00
+
+xrandr --output HDMI1 --left-of HDMI2 # set left-right-of position of two screens
